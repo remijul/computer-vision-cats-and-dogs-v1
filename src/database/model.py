575a2ba5e76ea527b92e0ef0aca42f5c720f7ecf
VestiC1 <steve.dos.santos.41@gmail.com>
2025-09-25 14:08:26 +0200

    modification id optional

--- src/database/model.py
+++ src/database/model.py
@@ -1,5 +1,6 @@
 from sqlmodel import Field, SQLModel
 from datetime import datetime
+from typing import Optional
 
 class Feedback(SQLModel, table=True):
     id: int = Field(primary_key=True)
@@ -9,7 +10,8 @@ class Feedback(SQLModel, table=True):
     last_modified: datetime
 
 class Monitoring(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    # Permet à SQLModel de gérer l’auto-incrément :
+    id: Optional[int] = Field(default=None, primary_key=True)
     timestamp: datetime
     inference_time: float
     succes: bool
